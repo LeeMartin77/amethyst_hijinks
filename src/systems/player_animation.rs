27f00f75df01b1bbcc21633e8da9ad3dac2a6817
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-14 21:05:48 +0100

    WIP: Lets stash this for now, and go try Godot

--- src/systems/player_animation.rs
+++ src/systems/player_animation.rs
@@ -7,18 +7,32 @@ use crate::entities::{player_character::PlayerCharacter};
 // Legion RFC is the thing to read 
 // https://github.com/amethyst/rfcs/issues/22
 
-pub struct PlayerAnimationSystem {
+pub enum SpriteFacing {
+    Left,
+    Right
+}
+
+pub struct PlayerIdleAnimationData
+{
     idle_frames: [u8; 4],
     idle_frame_index: u8,
     idle_time: f32
 }
 
+pub struct PlayerAnimationSystem {
+    sprite_facing: SpriteFacing,
+    idle_animation_data: PlayerIdleAnimationData,
+}
+
 impl PlayerAnimationSystem {
     pub fn new() -> PlayerAnimationSystem {
         PlayerAnimationSystem {
-            idle_frames: [40, 41, 42, 43],
-            idle_frame_index: 0,
-            idle_time: 0.0
+            sprite_facing: SpriteFacing::Right,
+            idle_animation_data: PlayerIdleAnimationData {
+                idle_frames: [40, 41, 42, 43],
+                idle_frame_index: 0,
+                idle_time: 0.0
+            }
         }
     }
 }
@@ -33,18 +47,20 @@ impl System for PlayerAnimationSystem {
             .read_resource::<Time>()
             .build(move | _commands, world, time, query_objects | {
                 for (_player_char, mut render) in query_objects.iter_mut(world) {
-                    if self.idle_time > 0.4 {
-                        self.idle_time = 0.0;
-                        self.idle_frame_index = self.idle_frame_index + 1;
-                        if self.idle_frame_index as usize > self.idle_frames.len() - 1 {
-                            self.idle_frame_index = 0;
+                    if self.idle_animation_data.idle_time > 0.4 {
+                        self.idle_animation_data.idle_time = 0.0;
+                        self.idle_animation_data.idle_frame_index = self.idle_animation_data.idle_frame_index + 1;
+                        if self.idle_animation_data.idle_frame_index as usize > self.idle_animation_data.idle_frames.len() - 1 {
+                            self.idle_animation_data.idle_frame_index = 0;
                         }
-                        render.sprite_number = self.idle_frames[self.idle_frame_index as usize].into();
+                        render.sprite_number = self.idle_animation_data.idle_frames[self.idle_animation_data.idle_frame_index as usize].into();
                     }
                     else
                     {
-                        self.idle_time = self.idle_time + time.delta_seconds();
+                        self.idle_animation_data.idle_time = self.idle_animation_data.idle_time + time.delta_seconds();
                     }
+                    
+                    render.
                 }
             })
         )
